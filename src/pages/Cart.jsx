fdd4111c7b0e23f351dd98a026bbe0e752d8eca3
ravib7 <ravibchaudhari7@gmail.com>
2025-01-15 00:03:08 +0530

    Automatic commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,12 +3,13 @@ import Layout from '../components/layouts/Layout'
 import Brocoli from "../assets/img/vegetable-item-2.jpg"
 import Potatoes from "../assets/img/vegetable-item-5.jpg"
 import Banana from "../assets/img/vegetable-item-3.png"
+import "../assets/styles/pages/cart.css"
 
 const Cart = () => {
 
-    const [incressQuantity, setIncressQuantity] = useState(0)
-    const [oneIncressQuantity, setOneIncressQuantity] = useState(0)
-    const [twoIncressQuantity, setTwoIncressQuantity] = useState(0)
+    const [incressQuantity, setIncressQuantity] = useState(1)
+    const [oneIncressQuantity, setOneIncressQuantity] = useState(1)
+    const [twoIncressQuantity, setTwoIncressQuantity] = useState(1)
 
     const incress = () => {
         setIncressQuantity(incressQuantity + 1)
@@ -62,14 +63,14 @@ const Cart = () => {
                                     <td className='py-4'>₹195</td>
                                     <td className='py-4'>
                                         <h6 style={{ userSelect: "none" }}>
-                                            <i class="bi bi-dash-circle fs-3 me-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={decrees}></i>
+                                            <i class="bi bi-dash fs-4 fw-bolder shadow-sm me-3 d-none d-md-inline-flex" onClick={decrees}></i>
                                             {incressQuantity}
-                                            <i class="bi bi-plus-circle fs-3 ms-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={incress}></i>
+                                            <i class="bi bi-plus fs-4 shadow-sm ms-3 d-none d-md-inline-flex" onClick={incress}></i>
                                         </h6>
                                     </td>
                                     <td className='py-4'>₹130</td>
                                     <td className='py-3'>
-                                        <i class="bi bi-x-circle fs-2 text-danger" style={{ cursor: 'pointer' }}></i>
+                                        <i class="bi bi-x fs-4 shadow-sm text-danger mt-2"></i>
                                     </td>
                                 </tr>
                                 <tr>
@@ -80,14 +81,14 @@ const Cart = () => {
                                     <td className='py-4'>₹50</td>
                                     <td className='py-3'>
                                         <h6 style={{ userSelect: "none" }}>
-                                            <i class="bi bi-dash-circle fs-3 me-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={oneDecrees}></i>
+                                            <i class="bi bi-dash fs-4 fw-bolder shadow-sm me-3 d-none d-md-inline-flex" onClick={oneDecrees}></i>
                                             {oneIncressQuantity}
-                                            <i class="bi bi-plus-circle fs-3 ms-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={oneIncress}></i>
+                                            <i class="bi bi-plus fs-4 shadow-sm ms-3 d-none d-md-inline-flex" onClick={oneIncress}></i>
                                         </h6>
                                     </td>
                                     <td className='py-4'>₹50</td>
                                     <td className='py-3'>
-                                        <i class="bi bi-x-circle fs-2 text-danger" style={{ cursor: 'pointer' }}></i>
+                                        <i class="bi bi-x fs-4 shadow-sm text-danger mt-2"></i>
                                     </td>
                                 </tr>
                                 <tr>
@@ -98,22 +99,58 @@ const Cart = () => {
                                     <td className='py-4'>₹55</td>
                                     <td className='py-4'>
                                         <h6 style={{ userSelect: "none" }}>
-                                            <i class="bi bi-dash-circle fs-3 me-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={twoDecrees}></i>
+                                            <i class="bi bi-dash fs-4 fw-bolder shadow-sm me-3 d-none d-md-inline-flex" onClick={twoDecrees}></i>
                                             {twoIncressQuantity}
-                                            <i class="bi bi-plus-circle fs-3 ms-3" style={{ cursor: 'pointer', color: "#81c408" }} onClick={twoIncress}></i>
+                                            <i class="bi bi-plus fs-4 shadow-sm ms-3 d-none d-md-inline-flex" onClick={twoIncress}></i>
                                         </h6>
                                     </td>
                                     <td className='py-4'>₹55</td>
                                     <td className='py-3'>
-                                        <i class="bi bi-x-circle fs-2 text-danger" style={{ cursor: 'pointer' }}></i>
+                                        <i class="bi bi-x fs-4 shadow-sm text-danger mt-2"></i>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
-                    <div className="col-md-12 col-lg-4"></div>
-                    <div className="col-md-12 col-lg-4"></div>
-                    <div className="col-md-12 col-lg-4"></div>
+                </div>
+
+                <div className="row gap-5">
+                    <div className="col-md-6 col-lg-6">
+                        <div className="row">
+                            <div className="col-md-6 col-lg-6">
+                                <input
+                                    type="number"
+                                    class="form-control border-0 border-bottom rounded-0 shadow-none mt-5"
+                                    placeholder="Enter Coupon Code"
+                                />
+                            </div>
+                            <div className="col-md-6 col-lg-6">
+                                <button className="btn cart-btn fw-semibold rounded-pill px-4 mt-4" type="button">Apply Coupon</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-5 col-lg-5 mt-5">
+                        <div class="card mb-5">
+                            <div class="card-header fs-2 fw-semibold" style={{ backgroundColor: "#f4f6f8" }}><span className='fs-2 me-2 fw-bold'>Cart</span>Total</div>
+                            <div class="card-body" style={{ backgroundColor: "#f4f6f8" }}>
+                                <div className="d-flex justify-content-between align-content-center">
+                                    <h5>Subtotal :</h5>
+                                    <p>₹500</p>
+                                </div>
+                                <div className="d-flex justify-content-between align-content-center mt-3">
+                                    <h5>Shipping :</h5>
+                                    <p>Flat rate: ₹300 <br /> Shipping to Mumbai.</p>
+                                </div>
+                            </div>
+                            <div class="card-footer" style={{ backgroundColor: "#f4f6f8" }}>
+                                <div className="d-flex justify-content-between align-content-center mt-3">
+                                    <h5>Total :</h5>
+                                    <p>₹1000</p>
+                                </div>
+                                <button className="btn cart-btn fw-semibold rounded-pill px-4 mt-4 mb-3" type="button">PROCEED CHECKOUT</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout >
