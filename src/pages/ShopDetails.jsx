b0f38ae635bcfbd865894faa5ac51dcc07a46a02
ravib7 <ravibchaudhari7@gmail.com>
2025-01-23 00:08:32 +0530

    Automatic commit

--- src/pages/ShopDetails.jsx
+++ src/pages/ShopDetails.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import Layout from '../components/layouts/Layout'
-import Brocoli from "../assets/img/single-item.jpg"
 import '../../src/assets/styles/components/checkout.css'
 import { Link } from 'react-router-dom'
+import Brocoli from "../assets/img/single-item.jpg"
+import Apple from "/src/assets/img/featur-1.jpg"
+import Strawberry from "/src/assets/img/featur-2.jpg"
+import Cauliflower from "/src/assets/img/featur-3.jpg"
+import Capsicum from "/src/assets/img/vegetable-item-4.jpg"
+import Potato from "/src/assets/img/vegetable-item-5.jpg"
+import Coriander from "/src/assets/img/vegetable-item-6.jpg"
 
 
 const ShopDetails = () => {
@@ -21,7 +27,7 @@ const ShopDetails = () => {
     return <>
         <Layout>
             <div className="container my-5">
-                <div className="row">
+                <div className="row gap-4">
                     <div className="col-md-12 col-lg-4">
                         <img className='img-fluid rounded-3' style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }} src={Brocoli} alt="" />
                     </div>
@@ -55,7 +61,7 @@ const ShopDetails = () => {
                         </Link>
                     </div>
 
-                    <div className="col-md-12 col-lg-4">
+                    <div className="col-md-12 col-lg-3">
                         <div class="input-group mb-3">
                             <input type="text" class="form-control py-3" placeholder="keywords" aria-label="Username" aria-describedby="basic-addon1" />
                             <span class="input-group-text" id="basic-addon1">
@@ -84,28 +90,126 @@ const ShopDetails = () => {
 
                         <p className='fs-4 fw-semibold'>Featured products</p>
 
-                        {/* <div>
-                            <div class="card mb-5">
-                                <div class="card-body">
-                                    <div className="row">
-                                        <div className="col-md-12 col-lg-6"></div>
-                                        <div className="col-md-12 col-lg-6">
-                                            <p>Big Banana</p>
-                                            <p>
-                                                <span class="text-warning"><i class="fas fa-star"></i></span>
-                                                <span class="text-warning"><i class="fas fa-star"></i></span>
-                                                <span class="text-warning"><i class="fas fa-star"></i></span>
-                                                <span class="text-warning"><i class="fas fa-star"></i></span>
-                                                <span class="text-warning"><i class="fas fa-star"></i></span>
-                                            </p>
-                                            <h3>2.99 $
-                                                4.11 $
-                                            </h3>
-                                        </div>
-                                    </div>
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Apple} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Big Banana</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">180₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">200₹</h5>
                                 </div>
                             </div>
-                        </div> */}
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Strawberry} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Strawberry</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">500₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">535₹</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Cauliflower} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Brocoli</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">180₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">195₹</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Capsicum} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Capsicum</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">95₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">120₹</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Potato} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Potato</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">50₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">70₹</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="rounded" style={{ width: "100px", height: "100px" }}>
+                                <img className='img-fluid rounded' src={Coriander} alt="" />
+                            </div>
+                            <div className='ms-4'>
+                                <h6 class="mb-2">Coriander</h6>
+                                <div class="d-flex mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star" style={{ color: "gray" }}></i>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <h5 class="fw-bold me-2 text-secondary">80₹</h5>
+                                    <h5 class="text-danger text-decoration-line-through">100₹</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="btn checkout-btn fw-semibold w-100 py-3 px-5 my-3 rounded-5" style={{ border: "1px solid #ffb524" }} type="submit">View More</button>
                     </div>
                 </div>
             </div>
